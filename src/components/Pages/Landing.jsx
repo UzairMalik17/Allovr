60ca33d28a4b1652989902f2be9579f8fce92c02
UzairMalik17 <uzairam17@gmail.com>
2023-07-30 22:38:56 +0500

    Working

--- src/components/Pages/Landing.jsx
+++ src/components/Pages/Landing.jsx
@@ -4,6 +4,7 @@ import Hero from "../home/Hero";
 import Staking from "../home/Staking";
 import StakingInfo from "../home/StakingInfo";
 import Action from "../home/Action";
+import Working from "../home/Working";
 function Landing() {
   return (
     <div>
@@ -12,6 +13,7 @@ function Landing() {
       <Staking />
       <StakingInfo />
       <Action />
+      <Working />
     </div>
   );
 }
--- src/components/home/Action.jsx
+++ src/components/home/Action.jsx
@@ -7,10 +7,10 @@ function Action() {
     <ComponentWrapper style={`py-12`}>
       <div className="w-full items-center flex flex-col">
         <div className="w-11/12 h-[418px] min-[507px]:h-[358px] md:h-[300px]  flex flex-col relative rounded-2xl">
-          <div className="w-full h-[70px] absolute top-0 bg-gradient-to-t from-Yellow to-black -z-10"></div>
+          <div className="w-full h-[70px] absolute top-0 bg-gradient-to-t from-black to-black -z-10"></div>
           <div className="w-[60px] h-[100px] absolute rotate-[30deg] bg-black top-[-45px] left-[-30px] -z-10"></div>
           <div className="w-[60px] h-[100px] absolute rotate-[-30deg] bg-black top-[-45px] right-[-30px] -z-10"></div>
-          <div className="w-full flex flex-col gap-8 items-center mt-auto bg-Yellow py-10 rounded-2xl">
+          <div className="w-full flex flex-col gap-8 items-center mt-auto bg-Yellow py-10 shadow-[0px_-60px_40px_-20px_rgba(0,0,0,0.3)] shadow-Yellow rounded-2xl">
             <div className="w-full items-center text-center">
               <span className="w-full font-semibold text-[40px] md:text-[55px] font-workSans">
                 Start your action now!
