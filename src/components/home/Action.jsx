d325f91498d54e320e84a791de309b3d23de3619
UzairMalik17 <uzairam17@gmail.com>
2023-08-03 20:59:37 +0500

    Code refactored

--- src/components/home/Action.jsx
+++ src/components/home/Action.jsx
@@ -6,7 +6,7 @@ function Action() {
   return (
     <ComponentWrapper style={`py-12`}>
       <div className="w-full items-center flex flex-col">
-        <div className="w-11/12 h-[418px] min-[507px]:h-[358px] md:h-[300px]  flex flex-col relative rounded-2xl">
+        <div className="w-11/12 h-[418px] min-[507px]:h-[358px] md:h-[300px] flex flex-col relative rounded-2xl">
           <div className="w-full h-[70px] absolute top-0 bg-gradient-to-t from-black to-black -z-10"></div>
           <div className="w-[60px] h-[100px] absolute rotate-[30deg] bg-black top-[-45px] left-[-30px] -z-10"></div>
           <div className="w-full flex flex-col gap-8 items-center mt-auto bg-Yellow py-10 shadow-[0px_-60px_40px_-20px_rgba(0,0,0,0.3)] shadow-Yellow rounded-2xl">
@@ -15,10 +15,10 @@ function Action() {
                 Start your action now!
               </span>
             </div>
-            <div className="w-full flex flex-col md:flex-row gap-6 text-[16px] items-center justify-center font-spaceGrotesk">
+            <div className="w-full flex flex-col md:flex-row gap-6 items-center justify-center">
               <ImgButton
                 style={
-                  "w-10/12 md:w-[221px] h-[56px] flex flex-row-reverse hover:bg-Red font-normal bg-[#1A1A1A]"
+                  "w-10/12 md:w-[221px] h-[56px] flex flex-row-reverse hover:bg-Red bg-[#1A1A1A]"
                 }
                 alt={"Arrow"}
                 Img={"./images/arrow-down.png"}
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -5,17 +5,17 @@ import ImgButton from "../shared/ImgButton";
 function Hero() {
   return (
     <ComponentWrapper
-      style={`font-workSans bg-[url('./images/bottomGr.png')] min-[850px]:bg-Gradient bg-center bg-no-repeat bg-cover relative`}>
-      <div className="w-full h-[1000px] min-[850px]:h-screen flex flex-col gap-4 min-[850px]:flex-row font-bold text-[56px] min-[850px]:text-8xl leading-[61px] min-[850px]:leading-[105.6px] items-center justify-between">
+      style={`font-workSans bg-[url('./images/bottomGr.png')] min-[850px]:bg-Gradient bg-center bg-no-repeat bg-cover text-white relative`}>
+      <div className="w-full h-[1000px] min-[850px]:h-screen flex flex-col gap-4 min-[850px]:flex-row text-[56px] min-[850px]:text-8xl leading-[61px] min-[850px]:leading-[105px] items-center justify-between">
         <div className="w-full min-[850px]:w-[400px] h-[400px] min-[850px]:h-full flex flex-col gap-4 pt-[100px] items-center min-[850px]:items-start text-center">
-          <span className="text-white">Stake</span>{" "}
-          <span className="text-Yellow">ALLOVR.</span>
-          <span className="text-white w-full min-[850px]:w-[400px] leading-[30px] text-[20px] min-[850px]:text-3xl">
+          <span className="font-bold">Stake</span>{" "}
+          <span className="text-Yellow font-bold">ALLOVR.</span>
+          <span className="w-full min-[850px]:w-[400px] leading-[30px] text-[20px] min-[850px]:text-3xl">
             This is the best time to get the earnings you dream of.
           </span>
           <ImgButton
             style={
-              "w-[180px] h-[56px] text-[16px] flex flex-row-reverse justify-center hover:bg-Red font-spaceGrotesk font-normal"
+              "w-[180px] h-[56px] text-[16px] flex flex-row-reverse hover:bg-Red"
             }
             alt={"Arrow"}
             Img={"./images/arrow-down.png"}
